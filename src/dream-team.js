38e40ffe901eefa8cd36e0369b9f985a43930112
Natein <natein@gmail.com>
2020-02-24 18:28:12 +0200

    feat: add task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+    let res = [];
+    let len = arr.length;
+    for(let i = 0; i < len; i++) {
+        if(typeof(arr[i]) == "string") {
+            let str_len = arr[i].length;
+            for(let j = 0; j < str_len; j++) {
+                if(arr[i][j] != " ") {
+                    res.push(arr[i][j].toUpperCase());
+                    break;
+                }
+            }
+        }           
+    }
+    if(res.length == "") return false;
+    res.sort();
+    return res.join("");
 };
