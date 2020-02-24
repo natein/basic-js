5eaeac392e6a87ff6b6bdfd276b6b1ec5bbb700a
Natein <natein@gmail.com>
2020-02-24 20:27:25 +0200

    feat: add task solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(cur_date) {
+    if(arguments.length == 0) return 'Unable to determine the time of year!';
+    if(type(cur_date) != "date") throw new Error();
+    let month = cur_date.getMonth();
+    let seasons = ["winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "fall", "fall", "fall", "winter"];
+    return seasons[month];
 };
+
+function type(value) {
+  var regex = /^\[object (\S+?)\]$/;
+  var matches = Object.prototype.toString.call(value).match(regex) || [];
+  return (matches[1] || 'undefined').toLowerCase();
+}
