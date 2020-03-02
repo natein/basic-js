983632c71f73cfdb2806b2ab3184f0f4fc6af371
Natein <natein@gmail.com>
2020-03-02 02:21:59 +0200

    feat: add task solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,7 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+const SECONDS_IN_HOUR = 3600;
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let turns = 2 ** disksNumber - 1;    
+    turnsSpeed = turnsSpeed / SECONDS_IN_HOUR;
+    let seconds =  turns / turnsSpeed;
+    return {turns, seconds};
 }
