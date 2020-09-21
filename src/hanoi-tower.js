874e67435de7bbe03b566ea74da92518e4eb4330
Natein <natein@gmail.com>
2020-09-21 21:20:41 +0300

    fix: round error

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,7 @@
 const SECONDS_IN_HOUR = 3600;
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    let turns = 2 ** disksNumber - 1;    
-    turnsSpeed = turnsSpeed / SECONDS_IN_HOUR;
-    let seconds =  turns / turnsSpeed;
-    return {turns, seconds};
+  const turns = 2 ** disksNumber - 1;    
+  turnsSpeed = turnsSpeed / SECONDS_IN_HOUR;
+  const seconds = Math.floor(turns / turnsSpeed);
+  return {turns, seconds};
 }
